e5001ee977f4936cfe9b90e239f6d1f13e341f3c
Jasper Schelfhout <jasper.schelfhout@altabo.com>
2025-12-31 16:06:25 +0100

    Update pull_code.sh

--- pull_code.sh
+++ pull_code.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-cd \$HOME/localcart-node
+cd $HOME/localcart-node
 git fetch origin
 git reset --hard origin/main
 git clean -fd -e device-id.txt -e run.log
 
 echo "Code pull complete. Rebooting…"
-sudo reboot 
+sudo reboot
